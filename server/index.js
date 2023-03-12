3f88942665502462b137dba3871d9b5c071a9a14
lemmoor <49235295+lemmoor@users.noreply.github.com>
2023-03-12 17:07:33 +0100

    updated package.json and default port in index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const path = require('path')
+const express = require('express');
+const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
@@ -9,7 +11,7 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 io.on('connection', (socket) => {
     socket.on('join-room', (gameCode) => {
